09e1a83ce9b22b5ddf64d7c740e1ef91972a99da
Christoph Moser <cmoser@comscore.com>
2018-08-12 12:48:05 +0200

    setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+import os
+from setuptools import setup, find_packages
+
+setup(
+  name="cmml",
+  description="Useful utils for machine learning notebooks.",
+  author="Christoph Moser",
+  packages=find_packages()
+)
